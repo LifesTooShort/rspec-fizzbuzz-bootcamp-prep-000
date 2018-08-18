cf61abf9f87e7e53387797ed80f3a7dc4742fec0
Erin Penner <epenner0522@gmail.com>
2018-08-18 23:57:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,14 @@
 def fizzbuzz(number)
-
+  result = ""
+  
   if number % 3 == 0
     result = result + "Fizz"
   end
   if number % 5 == 0
     result = result + "Buzz"
+  end
+  if number % 3 != 0 && number % 5 != 0
+    result = nil
   end
   return result
 end
